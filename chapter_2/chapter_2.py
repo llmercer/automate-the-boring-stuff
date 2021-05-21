bafe4e7645984e62d1eb2f8396ea41656c0c5613
Lauren Mercer <laurenmercer67@gmail.com>
2021-05-20 21:42:45 -0600

    created python script to test elif statements and infinite loop

---
+++ chapter_2/chapter_2.py
@@ -0,0 +1,15 @@
+name = 'Lauren'
+
+age = 100
+
+if name == 'Alice':
+    print('Hi Alice')
+
+elif age < 12:
+    print("You are not Alice, kiddo.")
+
+elif age > 2000:
+    print('Unlike you, Alice is not an undead, immortal vampire.')
+
+elif age > 80: 
+    print('You are not Alice, grannie.')
